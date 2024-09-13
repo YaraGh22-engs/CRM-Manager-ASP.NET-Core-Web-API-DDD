7e4e8686fa55068ffa95ffd75baf6154e8bba272
YaraGh <yaraesmaiel123@gmail.com>
2024-09-13 14:32:24 -0500

    Application Layer / basics

---
+++ CRMManager.Application/Features/Customers/Commands/Create/CreateCustomerCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using CRMManager.Domain.Aggregates.CustomerAggregate;
+using CRMManager.Domain.Aggregates.CustomerAggregate.Repository;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRMManager.Application.Features.Customers.Commands.Create
+{
+    public class CreateCustomerCommandHandler : IRequestHandler<CreateCustomerCommand>
+    {
+        private readonly ICustomerRepository _customerRepository;
+        public CreateCustomerCommandHandler(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+        public async Task Handle(CreateCustomerCommand request, CancellationToken cancellationToken)
+        {
+            var customer = Customer.Create(request.Name, request.TaxNumber, request.Street);
+            await _customerRepository.AddAsync(customer); 
+        }
+    }
+}
---
+++ CRMManager.Application/Features/Customers/Queries/ListAll/ListAllCustomersQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using CRMManager.Application.Features.Customers.Queries.ListAll;
+using CRMManager.Domain.Aggregates.CustomerAggregate;
+using CRMManager.Domain.Aggregates.CustomerAggregate.Repository;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRMManager.Application.Features.Customers.Queries.ListAll
+{
+    public class ListAllCustomersQueryHandler : IRequestHandler<ListAllCustomersQuery, List<Customer>>
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public ListAllCustomersQueryHandler(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+
+
+        }
+
+        public async Task<List<Customer>> Handle(ListAllCustomersQuery request, CancellationToken cancellationToken)
+        {
+            return await _customerRepository.GetAllAsync();
+        }
+    }
+}
