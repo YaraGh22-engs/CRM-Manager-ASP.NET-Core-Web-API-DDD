c8d7fd9a404d6e532e7d29f30d5e7726a83017f1
YaraGh <yaraesmaiel123@gmail.com>
2024-09-21 05:49:40 -0500

    API layer / Basics

---
+++ CRMManager.Api/Controllers/CustomerController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using CRMManager.Application.Features.Customers.Queries.ListAll;
+using CRMManager.Application.Features.Customers.Commands.Create;
+
+namespace CRMManager.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public CustomerController(IMediator mediator  )
+        {
+            _mediator = mediator; 
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var query = new ListAllCustomersQuery();
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(string name, string taxNumber, string street)
+        {
+            var command = new CreateCustomerCommand(name, taxNumber, street);
+            await _mediator.Send(command);
+            return Ok();
+        }
+    }
+}
